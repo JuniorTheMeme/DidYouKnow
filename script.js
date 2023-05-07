c68b418666cb82802bcb5c6000231a9af1415e0f
JuniorTheMeme <87767229+JuniorTheMeme@users.noreply.github.com>
2023-05-07 17:22:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const todayday = document.querySelector('.day');
 const todaytime = document.querySelector('.time');
 const noofday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 
+
 function printDate(){
     var date = new Date();
     var day = date.getDate();
